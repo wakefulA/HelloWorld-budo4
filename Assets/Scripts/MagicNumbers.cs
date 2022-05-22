baafa9e5950c2ff3587688b10b53ecb48f6c11a8
Pavel <wakeful@mail.ru>
2022-05-22 03:12:09 +0300

    MagicNumbers

---
+++ Assets/Scripts/MagicNumbers.cs
@@ -0,0 +1,81 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class MagicNumbers : MonoBehaviour
+{
+    [SerializeField] private int _min = 1;
+    [SerializeField] private int _max = 1000;
+
+    public TextMeshProUGUI InfoLabel;
+    public TextMeshProUGUI InfoGuess;
+    public Button MoreButton;
+    public Button FinishButton;
+    public Button LessButton;
+    
+    private int _guess;
+
+    private void Start()
+    {
+
+        MoreButton.onClick.AddListener(MoreButtonClicked);
+        FinishButton.onClick.AddListener(FinishButtonClicked);
+        LessButton.onClick.AddListener(LessButtonClicked);
+        _min = 1;
+        _max = 1000;
+
+        
+        SetInfoText(($"Загадай число от {_min} до {_max}."));
+        CalculateGuess();
+    
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void CalculateGuess()
+    {
+        _guess = (_min + _max) / 2;
+
+        SetInfoText($"Твоё число: {_guess}?");
+    }
+
+    private void SetInfoText (string text)
+    {
+        Debug.Log(text);
+
+        InfoLabel.text = text; 
+    }
+
+    private void SetGuessText(string text)
+    {
+        Debug.Log(text);
+
+        InfoGuess.text = text; 
+    }
+
+
+    private void MoreButtonClicked()
+    {
+        SetInfoText("Число больше");
+        _min = _guess;
+        CalculateGuess();
+    }
+
+    private void LessButtonClicked()
+    {
+        SetInfoText("Число меньше");
+        _max = _guess;
+        CalculateGuess();
+    }
+
+    private void FinishButtonClicked()
+    {
+        SetInfoText($"Победа: твоё число {_guess}");
+    }
+    }
+
